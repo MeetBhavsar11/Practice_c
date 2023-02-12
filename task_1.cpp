e27ca5583a3767090933376ee64d286cee41d471
meetbhavsar11 <mitsbhavsar@gmail.com>
2023-02-12 16:06:52 +0530

    task_c

---
+++ task_1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	printf("\t\tMenu\n");
+	printf("\nProduct\t");
+	printf("\t\tPrice\n");
+	printf("\n1.Apple\t");
+	printf("\t\t90/-");
+	printf("\n2.Banana\t");
+	printf("\t80/-");
+	printf("\n3.Orange\t");
+	printf("\t80/-\n");
+}
---
+++ task_2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+	printf("\tYour Details\n");
+	printf("\nName :-\t");
+	printf("\tMitesh Bhavsar");
+	printf("\nEmail id :-\t");
+	printf("mitsbhavsar@gmail.com");
+	printf("\nDOB :-\t");
+	printf("\t1990/02/11");
+	printf("\nAge :-\t");
+	printf("\t32 Yrs");
+	printf("\nGender :-");
+	printf("\tMale");
+	printf("\nHobbies :-");
+	printf("\tTravelling");
+	printf("\nAddress :-");
+	printf("\tA/207 Omkar Residency, Vastral, Ahmedabad.");
+}
