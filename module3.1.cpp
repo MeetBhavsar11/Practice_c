da571266f39d8bcae2b86457ac56455e07388033
meetbhavsar11 <mitsbhavsar@gmail.com>
2023-03-27 21:24:14 +0530

    task

---
+++ module3.1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+
+{
+	printf("Mitesh Bhavsar\n");
+	
+	printf("\n11/02/1990\n");
+	
+	printf("\n32\n");
+	
+	printf("\nA1-207 Omkar Residency, Arpan school road, Vastral, Ahmedabad\n");
+	
+	return 0;
+}
